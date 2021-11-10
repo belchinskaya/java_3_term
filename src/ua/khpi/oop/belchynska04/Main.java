919ab51799bcb5e83b456fb7a10faeb05f921b26
belchinskaya <k.belchinskaya@gmail.com>
2021-11-10 23:36:29 +0200

    lab4 menu

--- src/ua/khpi/oop/belchynska04/Main.java
+++ src/ua/khpi/oop/belchynska04/Main.java
@@ -16,33 +16,34 @@ public final class Main {
 		
 	}
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		
-		Find_string m = new Find_string();
 		
 		
 		Menu menu = new Menu();
 		
+	      menu.menu();
+    }
+}
 
-		int quantity = 4;
-		
-		StringBuilder str1 = new StringBuilder(m.string1);
-		StringBuilder str2 = new StringBuilder(m.string2);
-		StringBuilder str3 = new StringBuilder(m.string3);
-		StringBuilder str4 = new StringBuilder(m.string4);
-		
-		int avarage_length = m.average_length(str1.length(), str2.length(), str3.length(), str4.length(), quantity);
-		
-		StringBuilder max_str[] = new StringBuilder[10];
-		StringBuilder min_str[] = new StringBuilder[10];
-
+class Find_string {
 	
-		
-		
-		m.find_group(str1, avarage_length, 0, 0, min_str, max_str);
-		m.find_group(str2, avarage_length, 1, 1, min_str, max_str);
-		m.find_group(str3, avarage_length, 2, 2, min_str, max_str);
-		m.find_group(str4, avarage_length, 3, 3, min_str, max_str);
+	
+	public Object get_str_from_console;
+	public String string1;
+	public String string2;
+	public String string3;
+	public String string4;
+	
+	public StringBuilder str1;
+	public StringBuilder str2;
+	public StringBuilder str3;
+	public StringBuilder str4;
+	
+	StringBuilder max_str[] = new StringBuilder[10];
+	StringBuilder min_str[] = new StringBuilder[10];
+	
+	final int quantity = 4;
+	public void print_group(){
 		
 		System.out.println("Strings whith length bigger than average");
 		for(int i = 0; i < 4; i++) {
@@ -57,21 +58,9 @@ public final class Main {
 			System.out.println(min_str[i] + "; string length:" + min_str[i].length() + "\n");
 			}
 		}
-		
-       
-    }
-}
-
-class Find_string {
-	public Object get_str_from_console;
-	public String string1;
-	public String string2;
-	String string3;
-	String string4;
-
+	}
 	public void find_group(StringBuilder str, int average_length, int pos_min, int pos_max, StringBuilder min_str[], StringBuilder max_str[]) {
 
-		
 		if(str.length() < average_length) {
 			get_string( str, pos_min, min_str);
 		
@@ -93,41 +82,77 @@ class Find_string {
 	}
 	
 	public  void get_str_from_console() {
-		System.out.println("Print 4 strings for making task:");
+		System.out.println("Print 4 strings for making task:\n");
 		Scanner scanner = new Scanner(System.in);
 		string1 = scanner.nextLine();
 		string2 = scanner.nextLine();
 		string3 = scanner.nextLine();
 		string4 = scanner.nextLine();
-
+		
+		str1 = new StringBuilder(string1);
+		str2 = new StringBuilder(string2);
+		str3 = new StringBuilder(string3);
+		str4 = new StringBuilder(string4);
+		
+	}
+	
+	/**
+	 * вивід на екран введених коритсувачем даних
+	 */
+	public void show_strings() {
+		System.out.println("You printed strings:\n" + str1 + "\n" + str2 + "\n" + str3 + "\n" + str4 + "\n");
 	}
 }
 
 class Menu{
-	Scanner scanner = new Scanner(System.in);
+
 	Find_string m = new Find_string();
+	String command;
+	int printed_num;
+	
 	
 	
 	public void menu(){
-
-		System.out.println("Press:\n 1 - to input your data,\n 2 - to show your input data,\n 3 - to make the task,\n 4 - to stop the program");
+		
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Press:\n 1 - to input your data,\n 2 - to show your input data,\n 3 - to make the task,\n 4 - to stop the program\n");
 	    
-		int printed_num = scanner.nextInt();
-
-	    switch (printed_num) {
-	        case 1:
+		
+		command = scanner.nextLine();
+	
+		
+	    switch (command) {
+	        case "1":
 	        	m.get_str_from_console();
 	            menu();
 	            break;
-	        case 2: 
+	        case "2": 
+	        	m.show_strings();
 	        	menu();
 	            break;
-	        case 3:
+	        case "3":
+	        	int average = m.average_length(m.str1.length(), m.str2.length(), m.str3.length(), m.str4.length(), m.quantity);
+	        	m.find_group(m.str1, average, 0, 0, m.min_str, m.max_str);
+	    		m.find_group(m.str2, average, 1, 1, m.min_str, m.max_str);
+	    		m.find_group(m.str3, average, 2, 2, m.min_str, m.max_str);
+	    		m.find_group(m.str4, average, 3, 3, m.min_str, m.max_str);
+	    		m.print_group();
 	        	menu();
 	            break;
-	        case 4:  
+	        case "4":  
+	            break;
+	        case "-d":
 	        	
-	            break;
+	        	break;
+	        	
+	        case "-h":
+	        	System.out.println("Автор: Катерина Бельчинська\n\nІндивідуальне завдання: Ввести декілька рядків.\n"
+	        			+ "Розбити на дві групи: рядки, довжина яких менша за середню; рядки, довжина яких не менше середньої.\n"
+	        			+ "Вивести рядки та їх довжину по групах.\n\n" + "В ході виконання даної програми взаємодія з програмним забезпеченням виконується через меню та обрані користувачем команди.\n\n");
+	        	menu();
+	        	break;
 	        default: System.out.println("\nYou enter incorrect number. Try one more time\n");
 	            menu();
 	    }
